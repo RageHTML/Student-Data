353c1716bdf07ae2274b1788261e788950cf3f92
Rage_JS <deyvidm24@gmail.com>
2026-01-30 08:07:55 -0300

    Adicionando os metodos quantitativos para descobrir Medias Medias de hroas estudas, menor hora, maior hora e o desvio de padrao para ter uma base

--- s.py
+++ s.py
@@ -2,4 +2,30 @@ import pandas as pd
 
 df = pd.read_csv("student.csv")
 
-print(df.describe())
+renomear = {
+    "Hours Studied":"horas_estudadas",
+    "Previous Scores":"pontuacao_anterior",
+    "Extracurricular Activities":"atividades_extracurricular",
+    "Sleep Hours":"horas_dormida",
+    "Performance Index":"desempenho",
+    "Sample Question Papers Practiced":"provas_praticadas"
+}
+
+df.rename(columns=renomear, inplace=True)
+
+
+# Medias de horas estudadas 
+media_horas = df["horas_estudadas"].mean()
+# Menor hora estudada 
+menor_hora = df["horas_estudadas"].min()
+# Maior Hora Estudada
+maior_hora = df["horas_estudadas"].max()
+# Desvio Padrao ( maioria em media )
+padrao_hora = df["horas_estudadas"].std()
+
+
+
+print(f"Medias de Horas Estudadas: {media_horas}")
+print(f"Maior Quantidade de Horas Estudadas: {maior_hora}")
+print(f"Menor Quantidade de Horas Estudadas: {menor_hora}")
+print(f"A Maioria estuda em media: {padrao_hora}")
